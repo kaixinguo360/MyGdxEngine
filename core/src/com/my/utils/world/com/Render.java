0841660daecc5c86d143544d67224980e5076d61
Kaixinguo <kaixinguo@kaixinguo.site>
2021-12-24 00:11:04 +0800

    Optimize Render Component: Add new constructor

--- core/src/com/my/utils/world/com/Render.java
+++ core/src/com/my/utils/world/com/Render.java
@@ -2,6 +2,7 @@ package com.my.utils.world.com;
 
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
 import com.my.utils.world.Component;
 import com.my.utils.world.Config;
 import com.my.utils.world.Loadable;
@@ -29,6 +30,11 @@ public class Render implements Component, Loadable.OnInit {
         init();
     }
 
+    public Render(ModelInstance modelInstance) {
+        this.modelInstance = modelInstance;
+        updateBoundingBox();
+    }
+
     @Override
     public void init() {
         this.modelInstance = new ModelInstance(renderModel.model);
@@ -36,4 +42,14 @@ public class Render implements Component, Loadable.OnInit {
         this.dimensions.set(renderModel.dimensions);
         this.radius = renderModel.radius;
     }
+
+    public void updateBoundingBox() {
+        if (this.modelInstance == null) throw new RuntimeException("ModelInstance is null");
+        this.modelInstance.model.calculateBoundingBox(boundingBox);
+        boundingBox.getCenter(center);
+        boundingBox.getDimensions(dimensions);
+        radius = dimensions.len() / 2f;
+    }
+
+    private static final BoundingBox boundingBox = new BoundingBox();
 }
