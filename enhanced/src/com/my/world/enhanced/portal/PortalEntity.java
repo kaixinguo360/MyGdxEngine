6087341a7756a733370852b3d0442d6bbe1cd427
Kaixinguo <kaixinguo@kaixinguo.site>
2022-08-07 14:04:18 +0800

    Optimize PortalEntity

--- enhanced/src/com/my/world/enhanced/portal/PortalEntity.java
+++ enhanced/src/com/my/world/enhanced/portal/PortalEntity.java
@@ -1,15 +1,20 @@
 package com.my.world.enhanced.portal;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.EllipseShapeBuilder;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
 import com.my.world.enhanced.entity.EnhancedEntity;
+import com.my.world.gdx.Matrix4Pool;
 import com.my.world.module.physics.Collision;
 import com.my.world.module.physics.RigidBody;
 import com.my.world.module.physics.rigidbody.SphereBody;
 import com.my.world.module.render.Render;
-import com.my.world.module.render.model.Sphere;
+import com.my.world.module.render.model.ProceduralModelRender;
 import net.mgsx.gltf.scene3d.attributes.PBRColorAttribute;
 
 public class PortalEntity extends EnhancedEntity {
@@ -21,8 +26,21 @@ public class PortalEntity extends EnhancedEntity {
 
     public PortalEntity(float radius) {
         setName("Portal");
-        Material material = new Material(PBRColorAttribute.createDiffuse(Color.RED));
-        render = addComponent(new Sphere(2 * radius, 2 * radius, 2 * radius, 16, 16, material, VertexAttributes.Usage.Position));
+        int Divisions = 16;
+        long Attributes = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal;
+        Material Material = new Material(PBRColorAttribute.createDiffuse(Color.RED));
+        float Radius = radius;
+        render = addComponent(new ProceduralModelRender() {{
+            mdBuilder.begin();
+            MeshPartBuilder part = mdBuilder.part("ellipse", GL20.GL_TRIANGLES, Attributes, Material);
+            Matrix4 tmpM = Matrix4Pool.obtain();
+            part.setVertexTransform(tmpM.rotate(Vector3.X, 90));
+            Matrix4Pool.free(tmpM);
+            EllipseShapeBuilder.build(part, Radius * 2, Radius * 2, 0, 0, Divisions, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 1, 0, 360f);
+            EllipseShapeBuilder.build(part, Radius * 2, Radius * 2, 0, 0, Divisions, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, 1, 180f, -180f);
+            model = mdBuilder.end();
+            init();
+        }});
         rigidBody = addComponent(new SphereBody(radius, 500f));
         rigidBody.isTrigger = true;
         collision = addComponent(new Collision(Collision.NORMAL_FLAG, Collision.ALL_FLAG));
--- enhanced/src/com/my/world/enhanced/portal/PortalScript.java
+++ enhanced/src/com/my/world/enhanced/portal/PortalScript.java
@@ -1,6 +1,7 @@
 package com.my.world.enhanced.portal;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -98,7 +99,7 @@ public class PortalScript implements ScriptSystem.OnStart, ScriptSystem.OnUpdate
         this.camera.up.set(cam.up).rot(tmpM);
 
         // 基于相机近剪裁平面实现传送门前方遮挡物体剔除 (问题: 不精确)
-        this.camera.near = -radius + targetTransform.getTranslation(tmpV).sub(this.camera.position).dot(this.camera.direction);
+        this.camera.near = targetTransform.getTranslation(tmpV).sub(this.camera.position).dot(this.camera.direction);
         if (this.camera.near <= 0) {
             this.camera.near = 0.01f;
         }
@@ -107,8 +108,39 @@ public class PortalScript implements ScriptSystem.OnStart, ScriptSystem.OnUpdate
 
         Matrix4Pool.free(tmpM);
         Vector3Pool.free(tmpV);
+
+//        // 基于斜近平面平截头体映射矩阵实现传送门前方遮挡物体剔除
+//        Matrix4 transform = selfPosition.getGlobalTransform();
+//
+//        Vector3 position = Vector3.Zero.cpy().mul(transform);
+//        Vector3 direction = Vector3.Z.cpy().scl(-1).rot(transform);
+//        Vector4 cameraSpaceClipPlane = cameraSpacePlane(camera, position, direction);
+//
+////        position.set(0, 0, -5);
+////        direction.set(0, 0, -1);
+////        cameraSpaceClipPlane = new Vector4(direction.x, direction.y, direction.z, -direction.dot(position));
+//
+//        calculateObliqueMatrix(camera.projection, cameraSpaceClipPlane);
+//        updateProjectionMatrix(camera);
+
+        Vector3 position = new Vector3(0.000001f, 0.000001f, -0.000001f);
+        Vector3 direction = new Vector3(0.000001f, 0.000001f, -1);
+        Vector4 cameraSpaceClipPlane = new Vector4(direction.x, direction.y, direction.z, -direction.dot(position));
+//        tmpM.set(cam.view).inv().tra();
+//        cameraSpaceClipPlane.traMul(tmpM);
+//        Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(Matrix4x4.Inverse(GetOriginalCamera().worldToCameraMatrix)) * clipPlaneWorldSpace;
+        cam.update();
+        calculateObliqueMatrix(cam.projection, cameraSpaceClipPlane);
+        updateProjectionMatrix(cam);
     }
 
+    public void updateProjectionMatrix(PerspectiveCamera camera) {
+        camera.combined.set(camera.projection).mul(camera.view);
+//        camera.invProjectionView.set(camera.combined).inv();
+//        camera.frustum.update(camera.invProjectionView);
+    }
+
+//    private Matrix4 tmpM = new Matrix4();
     private Matrix4 getTargetTransform() {
         Matrix4 targetTransform;
         if (targetPosition != null) {
@@ -303,79 +335,106 @@ public class PortalScript implements ScriptSystem.OnStart, ScriptSystem.OnUpdate
         }
     }
 
-//    // ----- 基于斜近平面平截头体映射矩阵实现传送门前方遮挡物体剔除 (废弃: 未测试) ----- //
-//
-//    public void onPreRender() {
-//        Vector4 cameraSpaceClipPlane = cameraSpacePlane(camera, new Vector3(0.0f, 0.0f, 0.0f), Vector3.Y, 1.0f);
-//        calculateObliqueMatrix(camera.projection, cameraSpaceClipPlane);
-//    }
-//
-//    public static Vector4 cameraSpacePlane(Camera cam, Vector3 pos, Vector3 normal, float sideSign) {
-//        tmpV1.set(normal).scl(0.07f).add(pos);
-//        tmpV1.mul(cam.view);
-//        Vector3 point = new Vector3(0.0f, 0.0f, 0.0f).mul(tmpM.set(cam.view).inv());
-//        tmpV1.sub(0.0f, point.y, 0.0f);
-//        tmpV2.set(normal).mul(cam.view).nor().scl(sideSign);
-//        return new Vector4(tmpV2.x, tmpV2.y, tmpV2.z, -tmpV1.dot(tmpV2));
-//    }
-//
-//    private final static Vector3 tmpV1 = new Vector3();
-//    private final static Vector3 tmpV2 = new Vector3();
-//    private final static Matrix4 tmpM = new Matrix4();
-//    public static void calculateObliqueMatrix(Matrix4 projection, Vector4 clipPlane) {
+    // ----- 基于斜近平面平截头体映射矩阵实现传送门前方遮挡物体剔除 (废弃: 未测试) ----- //
+
+    public static Vector4 cameraSpacePlane(Camera camera, Vector3 position, Vector3 direction) {
+        Matrix4 view = Matrix4Pool.obtain().set(camera.view);
+        position.mul(view);
+        direction.nor().rot(view);
+        Matrix4Pool.free(view);
+
+        if (direction.dot(Vector3.Z) > 0) {
+            direction.scl(-1);
+        }
+
+        return new Vector4(direction.x, direction.y, direction.z, -direction.dot(position));
+    }
+
+    public static void calculateObliqueMatrix(Matrix4 matrix, Vector4 clipPlane) {
 //        Vector4 q = new Vector4(
 //                sgn(clipPlane.x),
 //                sgn(clipPlane.y),
 //                1.0f,
 //                1.0f
-//        ).mul(tmpM.set(projection).inv());
-//        Vector4 c = clipPlane.scl(2.0f / clipPlane.dot(q));
+//        ).traMul(matrix.cpy().inv());
+//        Vector4 c = clipPlane.scl(2.0F / (clipPlane.dot(q)));
 //        // third row = clip plane - fourth row
-//        projection.val[2] = c.x - projection.val[3];
-//        projection.val[6] = c.y - projection.val[7];
-//        projection.val[10] = c.z - projection.val[11];
-//        projection.val[14] = c.w - projection.val[15];
-//    }
-//
-//    public static float sgn(float a) {
-//        if (a > 0.0f) return 1.0f;
-//        if (a < 0.0f) return -1.0f;
-//        return 0.0f;
-//    }
-//
-//    public static class Vector4 {
-//
-//        private float x;
-//        private float y;
-//        private float z;
-//        private float w;
-//
-//        public Vector4(float x, float y, float z, float w) {
-//            this.x = x;
-//            this.y = y;
-//            this.z = z;
-//            this.w = w;
-//        }
-//
-//        public Vector4 mul(Matrix4 matrix) {
-//            final float[] l_mat = matrix.val;
-//            x = x * l_mat[Matrix4.M00] + y * l_mat[Matrix4.M01] + z * l_mat[Matrix4.M02] + w * l_mat[Matrix4.M03];
-//            y = x * l_mat[Matrix4.M10] + y * l_mat[Matrix4.M11] + z * l_mat[Matrix4.M12] + w * l_mat[Matrix4.M13];
-//            x = x * l_mat[Matrix4.M20] + y * l_mat[Matrix4.M21] + z * l_mat[Matrix4.M22] + w * l_mat[Matrix4.M23];
-//            w = x * l_mat[Matrix4.M20] + y * l_mat[Matrix4.M21] + z * l_mat[Matrix4.M22] + w * l_mat[Matrix4.M23];
-//            return this;
-//        }
-//
-//        public float dot(Vector4 vector) {
-//            return x * vector.x + y * vector.y + z * vector.z + w * vector.w;
-//        }
-//
-//        public Vector4 scl(float f) {
-//            x = x * f;
-//            y = x * f;
-//            x = x * f;
-//            w = x * f;
-//            return this;
-//        }
-//    }
+//        matrix.val[2] = c.x - matrix.val[3];
+//        matrix.val[6] = c.y - matrix.val[7];
+//        matrix.val[10] = c.z - matrix.val[11];
+//        matrix.val[14] = c.w - matrix.val[15];
+
+        // Calculate the clip-space corner point opposite the clipping plane
+        // as (sgn(clipPlane.x), sgn(clipPlane.y), 1, 1) and
+        // transform it into camera space by multiplying it
+        // by the inverse of the projection matrix
+        Vector4 q = new Vector4(
+                (sgn(clipPlane.x) + matrix.val[8]) / matrix.val[0],
+                (sgn(clipPlane.y) + matrix.val[9]) / matrix.val[5],
+                -1.0F,
+                (1.0F + matrix.val[10]) / matrix.val[14]
+        );
+
+        // Calculate the scaled plane vector
+//        Vector4 c = clipPlane * (2.0F / Dot(clipPlane, q));
+        Vector4 c = clipPlane.scl(2.0F / clipPlane.dot(q));
+
+        // Replace the third row of the projection matrix
+        matrix.val[2] = c.x;
+        matrix.val[6] = c.y;
+        matrix.val[10] = c.z + 1.0F;
+        matrix.val[14] = c.w;
+
+    }
+
+    public static float sgn(float a) {
+        if (a > 0.0f) return 1.0f;
+        if (a < 0.0f) return -1.0f;
+        return 0.0f;
+    }
+
+    public static class Vector4 {
+
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public Vector4(float x, float y, float z, float w) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public Vector4 mul(Matrix4 matrix) {
+            final float[] l_mat = matrix.val;
+            x = x * l_mat[Matrix4.M00] + y * l_mat[Matrix4.M01] + z * l_mat[Matrix4.M02] + w * l_mat[Matrix4.M03];
+            y = x * l_mat[Matrix4.M10] + y * l_mat[Matrix4.M11] + z * l_mat[Matrix4.M12] + w * l_mat[Matrix4.M13];
+            z = x * l_mat[Matrix4.M20] + y * l_mat[Matrix4.M21] + z * l_mat[Matrix4.M22] + w * l_mat[Matrix4.M23];
+            w = x * l_mat[Matrix4.M30] + y * l_mat[Matrix4.M31] + z * l_mat[Matrix4.M32] + w * l_mat[Matrix4.M33];
+            return this;
+        }
+
+        public Vector4 traMul(Matrix4 inv) {
+            final float[] l_mat = inv.val;
+            x = x * l_mat[Matrix4.M00] + y * l_mat[Matrix4.M10] + z * l_mat[Matrix4.M20] + w * l_mat[Matrix4.M30];
+            y = x * l_mat[Matrix4.M01] + y * l_mat[Matrix4.M11] + z * l_mat[Matrix4.M21] + w * l_mat[Matrix4.M31];
+            z = x * l_mat[Matrix4.M02] + y * l_mat[Matrix4.M12] + z * l_mat[Matrix4.M22] + w * l_mat[Matrix4.M32];
+            w = x * l_mat[Matrix4.M03] + y * l_mat[Matrix4.M13] + z * l_mat[Matrix4.M23] + w * l_mat[Matrix4.M33];
+            return this;
+        }
+
+        public float dot(Vector4 vector) {
+            return x * vector.x + y * vector.y + z * vector.z + w * vector.w;
+        }
+
+        public Vector4 scl(float f) {
+            x = x * f;
+            y = x * f;
+            x = x * f;
+            w = x * f;
+            return this;
+        }
+    }
 }
