cbb508288e930dabb245f1d3115ef2f2a472342c
Kaixinguo <kaixinguo@kaixinguo.site>
2022-08-21 15:31:45 +0800

    Update game logic: Optimize CharacterEntity & Add Base Scene

--- demo/src/com/my/demo/entity/object/CharacterEntity.java
+++ demo/src/com/my/demo/entity/object/CharacterEntity.java
@@ -1,20 +1,25 @@
 package com.my.demo.entity.object;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.my.demo.entity.common.CharacterSwitcherAgent;
-import com.my.demo.entity.weapon.BulletEntity;
 import com.my.world.enhanced.entity.EnhancedEntity;
-import com.my.world.module.physics.TemplateRigidBody;
+import com.my.world.module.physics.rigidbody.CapsuleBody;
 import com.my.world.module.physics.script.EnhancedCharacterController;
-import com.my.world.module.render.model.GLTFModel;
-import com.my.world.module.render.model.GLTFModelInstance;
+import com.my.world.module.render.light.GLTFSpotLight;
+import com.my.world.module.render.model.Capsule;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import net.mgsx.gltf.scene3d.attributes.PBRColorAttribute;
 
 public class CharacterEntity extends EnhancedEntity {
+
+    public static final long ATTRIBUTES = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal;
 
     @NoArgsConstructor
     @AllArgsConstructor
@@ -22,7 +27,7 @@ public class CharacterEntity extends EnhancedEntity {
     public static class Param {
         @Builder.Default public Matrix4 transform = new Matrix4();
         @Builder.Default public boolean active = true;
-        @Builder.Default public float height = 2f;
+        @Builder.Default public float height = 1.8f;
         @Builder.Default public float velocity = 30;
         @Builder.Default public float mass = 0.5f;
         @Builder.Default public int keyUp = Input.Keys.W;
@@ -31,25 +36,31 @@ public class CharacterEntity extends EnhancedEntity {
         @Builder.Default public int keyRight = Input.Keys.D;
         @Builder.Default public int keyJump = Input.Keys.SPACE;
         public String characterName;
-        public GLTFModel model;
-        public TemplateRigidBody body;
     }
 
-    public final GLTFModelInstance render;
+    public final Capsule render;
     public final EnhancedCharacterController controller;
     public final CharacterSwitcherAgent characterSwitcherAgent;
     public final CameraEntity camera;
 
     public CharacterEntity(Param p) {
 
-        transform.translate(0, p.height / 2f, 0).rotate(Vector3.Y, 180).mul(p.transform);
+        float scale = p.height / 2f;
+        float radius = 0.4f * scale;
+        float height = 1.2f * scale;
+        float cameraY = 0.6f * scale;
+
+        transform.translate(0, p.height / 2, 0).rotate(Vector3.Y, 180).mul(p.transform);
         decompose();
 
-        render = addComponent(new GLTFModelInstance(BulletEntity.model));
+        render = addComponent(new Capsule(
+                radius, p.height, 16,
+                new Material(PBRColorAttribute.createBaseColorFactor(Color.WHITE)), ATTRIBUTES
+        ));
 
         controller = addComponent(new EnhancedCharacterController());
         controller.setActive(p.active);
-        controller.shape = BulletEntity.body;
+        controller.shape = new CapsuleBody(radius, height, 50f);
         controller.velocity = p.velocity;
         controller.mass = p.mass;
         controller.keyUp = p.keyUp;
@@ -67,8 +78,17 @@ public class CharacterEntity extends EnhancedEntity {
         camera.controller.yaw = -180;
         camera.controller.yawTarget = -180;
         camera.controller.recoverEnabled = false;
+        camera.controller.translate.set(0, 0, 0);
+        camera.controller.center.set(0, cameraY, 0);
         camera.setParent(this);
         camera.setName("camera");
         addEntity(camera);
+
+        EnhancedEntity spotLight = new EnhancedEntity();
+        spotLight.setName("spotLight");
+        spotLight.position.setLocalTransform(m -> m.setToTranslation(0, cameraY, 0));
+        spotLight.addComponent(new GLTFSpotLight(Color.WHITE.cpy(), Vector3.Z.cpy(), 10f, 300f, 60f, 30f));
+        spotLight.setParent(this);
+        addEntity(spotLight);
     }
 }
--- demo/src/com/my/demo/scene/AnimationScene.java
+++ demo/src/com/my/demo/scene/AnimationScene.java
@@ -2,27 +2,20 @@ package com.my.demo.scene;
 
 import com.badlogic.gdx.Input;
 import com.my.demo.entity.common.AnimationEntity;
-import com.my.demo.entity.object.CharacterEntity;
-import com.my.demo.entity.object.GroundEntity;
 import com.my.world.core.Component;
 import com.my.world.core.Entity;
 import com.my.world.core.Scene;
-import com.my.world.enhanced.builder.BaseBuilder;
 import com.my.world.module.animation.Animation;
 import com.my.world.module.animation.DefaultAnimationController;
 import com.my.world.module.input.InputSystem;
 
 import java.util.Map;
 
-public class AnimationScene extends BaseBuilder<AnimationScene> {
+public class AnimationScene extends BaseScene<AnimationScene> {
 
     @Override
     public Entity build(Scene scene, Map<String, Object> params) {
-        GroundEntity ground = new GroundEntity();
-        ground.addToScene(scene);
-
-        CharacterEntity character = new CharacterEntity(new CharacterEntity.Param());
-        character.addToScene(scene);
+        super.build(scene, params);
 
         AnimationEntity animationEntity = new AnimationEntity();
         animationEntity.addToScene(scene);
