c8ed13c8da159be959b78f038a86bac4a84db9fa
Kaixinguo <kaixinguo@kaixinguo.site>
2022-11-09 21:38:00 +0800

    Optimize Retarder

--- enhanced/src/com/my/world/enhanced/util/Retarder.java
+++ enhanced/src/com/my/world/enhanced/util/Retarder.java
@@ -1,45 +1,63 @@
 package com.my.world.enhanced.util;
 
-import com.badlogic.gdx.math.MathUtils;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public class Retarder {
+public class Retarder<E> {
 
     @Setter
     public float changeTime = 1;
 
     public final Supplier<Float> timeGetter;
-    public final Supplier<Float> valueGetter;
-    public final Consumer<Float> valueSetter;
+    public final Supplier<E> valueGetter;
+    public final Consumer<E> valueSetter;
+    public final LerpFunction<E> lerpFunction;
 
+    @Getter
     protected boolean isChanging = false;
     protected float timeSinceChangeBegin = 0;
-    protected float fromValue;
-    protected float toValue;
+    protected E fromValue;
+    protected E toValue;
+    protected Supplier<E> toValueGetter;
 
-    public Retarder(Supplier<Float> valueGetter, Consumer<Float> valueSetter) {
-        this(null, valueGetter, valueSetter);
+    public Retarder(Supplier<E> valueGetter, Consumer<E> valueSetter, LerpFunction<E> lerpFunction) {
+        this(null, valueGetter, valueSetter, lerpFunction);
     }
 
-    public Retarder(Supplier<Float> timeGetter, Supplier<Float> valueGetter, Consumer<Float> valueSetter) {
+    public Retarder(Supplier<Float> timeGetter, Supplier<E> valueGetter, Consumer<E> valueSetter, LerpFunction<E> lerpFunction) {
         this.timeGetter = timeGetter;
         this.valueGetter = valueGetter;
         this.valueSetter = valueSetter;
+        this.lerpFunction = lerpFunction;
     }
 
-    public void setValue(float value) {
+    public void setValue(E value) {
         setValue(value, changeTime);
     }
 
-    public void setValue(float value, float changeTime) {
+    public void setValue(E value, float changeTime) {
         this.isChanging = true;
         this.changeTime = changeTime;
         this.timeSinceChangeBegin = 0;
         this.fromValue = valueGetter.get();
         this.toValue = value;
+        this.toValueGetter = null;
+    }
+
+    public void setValue(Supplier<E> toValueGetter) {
+        setValue(toValueGetter, changeTime);
+    }
+
+    public void setValue(Supplier<E> toValueGetter, float changeTime) {
+        this.isChanging = true;
+        this.changeTime = changeTime;
+        this.timeSinceChangeBegin = 0;
+        this.fromValue = valueGetter.get();
+        this.toValue = null;
+        this.toValueGetter = toValueGetter;
     }
 
     public void update() {
@@ -50,12 +68,17 @@ public class Retarder {
 
     public void update(float deltaTime) {
         if (!isChanging) return;
+        if (toValueGetter != null) toValue = toValueGetter.get();
         if (timeSinceChangeBegin < changeTime) {
             timeSinceChangeBegin += deltaTime;
-            valueSetter.accept(MathUtils.lerp(fromValue, toValue, timeSinceChangeBegin / changeTime));
+            valueSetter.accept(lerpFunction.lerp(fromValue, toValue, timeSinceChangeBegin / changeTime));
         } else {
             isChanging = false;
             valueSetter.accept(toValue);
         }
+    }
+
+    public interface LerpFunction<E> {
+        E lerp(E fromValue, E toValue, float progress);
     }
 }
