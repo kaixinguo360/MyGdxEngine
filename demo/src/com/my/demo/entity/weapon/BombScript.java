f0987a19395771fcaaeaf2a243ed8de45a739ee2
Kaixinguo <kaixinguo@kaixinguo.site>
2022-11-28 22:10:26 +0800

    Update game logic: Optimize BombScript

--- demo/src/com/my/demo/entity/weapon/BombScript.java
+++ demo/src/com/my/demo/entity/weapon/BombScript.java
@@ -4,14 +4,19 @@ import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.my.world.core.Config;
 import com.my.world.core.Entity;
+import com.my.world.core.Scene;
 import com.my.world.enhanced.builder.EntityBuilder;
 import com.my.world.module.common.Position;
+import com.my.world.module.particle.ParticlesEffect;
 import com.my.world.module.physics.RigidBody;
 import com.my.world.module.physics.script.CollisionHandler;
+import com.my.world.module.render.Render;
+import com.my.world.module.script.ScriptSystem;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class BombScript extends CollisionHandler {
+public class BombScript extends CollisionHandler implements ScriptSystem.OnUpdate {
 
     private static final Vector3 tmpV1 = new Vector3();
     private static final Vector3 tmpV2 = new Vector3();
@@ -22,21 +27,53 @@ public class BombScript extends CollisionHandler {
     @Config
     public float triggerVelocity = 20;
 
+    @Config
+    public float waitTimeBeforeRemove = 10;
+
+    @Config
+    @Getter
+    protected boolean isExploited = false;
+
     @Override
     public void collision(Entity target) {
-        if (!target.contain(RigidBody.class)) return;
         if (checkVelocity(self, target)) {
-//            System.out.println("Bomb! " + self.getId() + " ==> " + target.getId());
-            Entity entity = explosionBuilder.build(scene);
-            Matrix4 entityTransform = entity.getComponent(Position.class).getLocalTransform();
-            self.getComponent(Position.class).getGlobalTransform(entityTransform);
-            scene.getEntityManager().getBatch().removeEntity(self.getId());
+            explosion();
         }
     }
 
-    private boolean checkVelocity(Entity self, Entity target) {
+    @Override
+    public void update(Scene scene, Entity entity) {
+        if (!isExploited) return;
+        waitTimeBeforeRemove -= scene.getTimeManager().getDeltaTime();
+        if (waitTimeBeforeRemove > 0) return;
+        scene.getEntityManager().getBatch().removeEntity(self.getId());
+    }
+
+    public void explosion() {
+//        System.out.println("Bomb! " + self.getId() + " ==> " + target.getId());
+        if (isExploited) return;
+        isExploited = true;
+        Entity entity = explosionBuilder.build(scene);
+        Matrix4 entityTransform = entity.getComponent(Position.class).getLocalTransform();
+        self.getComponent(Position.class).getGlobalTransform(entityTransform);
+        if (waitTimeBeforeRemove == 0) {
+            scene.getEntityManager().getBatch().removeEntity(self.getId());
+        } else {
+            Render render = self.getComponent(Render.class);
+            if (render != null) render.setActive(false);
+            RigidBody rigidBody = self.getComponent(RigidBody.class);
+            if (rigidBody != null) rigidBody.setActive(false);
+            ParticlesEffect particlesEffect = self.getComponent(ParticlesEffect.class);
+            if (particlesEffect != null) particlesEffect.stop();
+        }
+    }
+
+    protected boolean checkVelocity(Entity self, Entity target) {
         tmpV1.set(self.getComponent(RigidBody.class).body.getLinearVelocity());
-        tmpV2.set(target.getComponent(RigidBody.class).body.getLinearVelocity());
-        return tmpV1.sub(tmpV2).len() > triggerVelocity;
+        if (target.contain(RigidBody.class)) {
+            tmpV2.set(target.getComponent(RigidBody.class).body.getLinearVelocity());
+            tmpV1.sub(tmpV2);
+        }
+        return tmpV1.len() > triggerVelocity;
     }
 }
